d6cf06bde16f03d92fb9d0a7a40a2424e851537e
clay <5711623+Clay26@users.noreply.github.com>
2024-04-14 10:51:51 -0400

    Add better local tests

--- LCBot.py
+++ LCBot.py
@@ -51,6 +51,7 @@ class LCBot():
 
         self.logger.setLevel(logging.DEBUG)
         logging.getLogger('discord.DailyLC').setLevel(logging.DEBUG)
+        logging.getLogger('discord.TableCache').setLevel(logging.DEBUG)
 
     def register_events(self):
         @self.bot.event
@@ -66,7 +67,21 @@ class LCBot():
         async def localTest(ctx):
             if self.environment == 'development':
                 dailyLC = self.bot.get_cog('DailyLC')
+
+                # Testing cache loading
+                channelId = dailyLC.load_channel_cache(ctx.guild.id)
+                await ctx.send(f'Got channelId [{channelId}] from cache.')
+
+                if (channelId > 0):
+                    # Test cache saving
+                    dailyLC.save_channel_cache(ctx.guild.id, channelId)
+                    await ctx.send(f'Saved server [{ctx.guild.id}] with channelId [{channelId}] to cache.')
+                else:
+                    await ctx.send(f'Skipping server [{ctx.guild.id}] channelId cache saving since no channel is currently set.')
+
+                # Testing message retrieval
                 message = await dailyLC.get_daily_question_message()
+
                 await ctx.send(embed=message)
 
         @self.bot.command()
