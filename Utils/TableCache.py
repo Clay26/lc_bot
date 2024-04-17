607e2029f1587925c3b7dabf1388810d435dbee7
clay <5711623+Clay26@users.noreply.github.com>
2024-04-17 17:59:13 -0400

    Add simple local cache

--- Utils/TableCache.py
+++ Utils/TableCache.py
@@ -11,6 +11,7 @@ class TableCache(Generic[E]):
     tableName: str
     tableServiceClient: Optional[TableServiceClient]
     tableClient: Optional[TableClient]
+    localCache: Dict[str, Any]
     entityType: Type[E]
     logger: logging.Logger
 
@@ -18,6 +19,8 @@ class TableCache(Generic[E]):
         self.tableName = entityType.get_partition_key()
         self.tableServiceClient = None
         self.tableClient = None
+
+        self.localCache = {}
 
         self.entityType = entityType
 
@@ -40,19 +43,40 @@ class TableCache(Generic[E]):
             self.logger.debug(f'Trying to save entity to table [{self.tableName}]')
             self.tableClient.upsert_entity(mode=UpdateMode.MERGE, entity=obj.to_entity())
             self.logger.info(f'Successfully saved entity to table [{self.tableName}]')
+
+            self.save_to_local_cache(obj)
         except Exception as e:
             self.logger.error(f"Error saving to table [{self.tableName}]: {e}")
 
     def load_entity(self, rowKey: any) -> Optional[E]:
+        localEntity = self.load_from_local_cache(str(rowKey))
+        if localEntity is not None:
+            self.logger.info(f'Found row [{rowKey}] in local [{self.tableName}] cache!')
+            return localEntity
+        else:
+            self.logger.info(f'Row [{rowKey}] not found in local [{self.tableName}] cache!')
+
         try:
             self.logger.debug(f'Trying to pull from [{self.tableName}] cache.')
             data = self.tableClient.get_entity(partition_key=self.tableName, row_key=str(rowKey))
             self.logger.info(f'Found row [{rowKey}] in [{self.tableName}] cache!')
-            return self.entityType.from_entity(data)
+
+            entity = self.entityType.from_entity(data)
+            self.save_to_local_cache(entity)
+            return entity
         except ResourceNotFoundError:
-            self.logger.debug(f'Row [{rowKey}] does not exit in [{self.tableName}].')
+            self.logger.info(f'Row [{rowKey}] does not exit in [{self.tableName}].')
             return None
         except Exception as e:
             # Log an error message if something goes wrong
             self.logger.error(f"Failed to pull [{self.tableName}] for row [{rowKey}]: {e}", exc_info=True)
             return None
+
+    def save_to_local_cache(self, obj: E):
+        self.logger.debug('Saving to local cache.')
+        self.localCache[str(obj.RowKey)] = obj
+
+    def load_from_local_cache(self, rowKey: str) -> Optional[E]:
+        self.logger.debug('Loading from local cache.')
+        localEntity = self.localCache.get(rowKey, None)
+        return localEntity
