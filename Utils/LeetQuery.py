21f5780b5443e31513f0a1e758ba6d49f1de6cb2
clay <5711623+Clay26@users.noreply.github.com>
2024-04-16 23:07:20 -0400

    Remove unused query params

--- Utils/LeetQuery.py
+++ Utils/LeetQuery.py
@@ -15,25 +15,14 @@ class LeetQuery:
             query questionOfToday {
               activeDailyCodingChallengeQuestion {
                 date
-                userStatus
                 link
                 question {
                   acRate
                   difficulty
-                  freqBar
                   frontendQuestionId: questionFrontendId
-                  isFavor
                   paidOnly: isPaidOnly
-                  status
                   title
                   titleSlug
-                  hasVideoSolution
-                  hasSolution
-                  topicTags {
-                    name
-                    id
-                    slug
-                  }
                 }
               }
             }
