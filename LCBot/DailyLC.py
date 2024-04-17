d630bf5ccfc0b72d1b940fe5bab9a2c8dc8b29d6
clay <5711623+Clay26@users.noreply.github.com>
2024-04-16 23:36:54 -0400

    Add types to DailyLC

--- LCBot/DailyLC.py
+++ LCBot/DailyLC.py
@@ -16,8 +16,13 @@ difficultColor = {
         'hard': discord.Color.red()
         }
 
+
 class DailyLC(commands.Cog):
-    def __init__(self, bot, connectionString):
+    bot: commands.Bot
+    logger: logging.Logger
+    serverCache: TableCache
+
+    def __init__(self, bot: commands.Bot, connectionString: str):
         self.bot = bot
         self.daily_question_loop.start()
         self.logger = logging.getLogger('discord.DailyLC')
@@ -32,58 +37,57 @@ class DailyLC(commands.Cog):
     async def daily_question_loop(self):
         self.logger.debug("Preparing to send daily question.")
         await self.send_daily_question()
-        self.logger.info(f'Successfully sent daily LC message.')
+        self.logger.info("Successfully sent daily LC message.")
 
     async def send_daily_question(self):
-            message = await self.get_daily_question_message()
+        message = await self.get_daily_question_message()
 
-            for guild in self.bot.guilds:
-                try:
-                    channelId = self.load_channel_cache(guild.id)
-                    if (channelId > 0):
-                        self.logger.info(f'Channel [{channelId}] was returned from the cache.')
-                        channel = self.bot.get_channel(int(channelId))
-                        if (channel is not None):
-                            await channel.send(embed=message)
-                            self.logger.info(f'Successfully sent daily question with link [{link}] to server [{guild.id}].')
-                        else:
-                            self.logger.info(f'Failed to get channel object for server [{guild.id}].')
+        for guild in self.bot.guilds:
+            try:
+                channelId = self.load_channel_cache(guild.id)
+                if (channelId > 0):
+                    self.logger.info(f'Channel [{channelId}] was returned from the cache.')
+                    channel = self.bot.get_channel(int(channelId))
+                    if (channel is not None):
+                        await channel.send(embed=message)
+                        self.logger.info(f'Successfully sent daily question with link [{link}] to server [{guild.id}].')
                     else:
-                        self.logger.info(f'Skip sending daily question since no channel is configured for server [{guild.id}].')
-                except Exception as e:
-                    # Log an error message if something goes wrong
-                    self.logger.error(f"Failed to send daily question: {e}", exc_info=True)
+                        self.logger.info(f'Failed to get channel object for server [{guild.id}].')
+                else:
+                    self.logger.info(f'Skip sending daily question since no channel is configured for server [{guild.id}].')
+            except Exception as e:
+                # Log an error message if something goes wrong
+                self.logger.error(f"Failed to send daily question: {e}", exc_info=True)
 
-    async def get_daily_question_message(self):
-            self.logger.debug("Querying LeetCode for daily question.")
+    async def get_daily_question_message(self) -> discord.Embed:
+        self.logger.debug("Querying LeetCode for daily question.")
 
-            question = await LeetQuery().daily_question()
+        question = await LeetQuery().daily_question()
 
-            link = question['activeDailyCodingChallengeQuestion']['link']
-            fullLink = f'https://leetcode.com{link}'
+        link = question['activeDailyCodingChallengeQuestion']['link']
+        fullLink = f'https://leetcode.com{link}'
 
-            difficulty = question['activeDailyCodingChallengeQuestion']['question']['difficulty']
-            acRate = question['activeDailyCodingChallengeQuestion']['question']['acRate']
-            problem = question['activeDailyCodingChallengeQuestion']['question']['title']
-            isPaidOnly = question['activeDailyCodingChallengeQuestion']['question']['paidOnly']
+        difficulty = question['activeDailyCodingChallengeQuestion']['question']['difficulty']
+        acRate = question['activeDailyCodingChallengeQuestion']['question']['acRate']
+        problem = question['activeDailyCodingChallengeQuestion']['question']['title']
+        isPaidOnly = question['activeDailyCodingChallengeQuestion']['question']['paidOnly']
 
-            title = "Daily LC"
-            date = datetime.datetime.now().strftime("%m-%d-%Y")
-            description = f'Gooooood morning grinders. Here is your question of the day for {date}! Once you solve, react to this message with the ✅ to add to your stats.'
-            color = difficultColor[difficulty.lower()]
-            fields = []
-            embedMessage = discord.Embed(title=title, 
-                                         description=description, 
-                                         url=fullLink, 
-                                         color=color)
-            embedMessage.add_field(name="Problem", value=f'{problem}')
-            embedMessage.add_field(name="Acceptance Rate", value=f'{acRate:.2f}%')
-            embedMessage.add_field(name="Difficulty", value=f'{difficulty}')
-            embedMessage.add_field(name="Is Paid Only", value=f'{isPaidOnly}')
+        title = "Daily LC"
+        date = datetime.datetime.now().strftime("%m-%d-%Y")
+        description = f'Gooooood morning grinders. Here is your question of the day for {date}! Once you solve, react to this message with the ✅ to add to your stats.'
+        color = difficultColor[difficulty.lower()]
+        embedMessage = discord.Embed(title=title,
+                                     description=description,
+                                     url=fullLink,
+                                     color=color)
+        embedMessage.add_field(name="Problem", value=f'{problem}')
+        embedMessage.add_field(name="Acceptance Rate", value=f'{acRate:.2f}%')
+        embedMessage.add_field(name="Difficulty", value=f'{difficulty}')
+        embedMessage.add_field(name="Is Paid Only", value=f'{isPaidOnly}')
 
-            return embedMessage
+        return embedMessage
 
-    async def set_channel_id(self, ctx, channelId):
+    async def set_channel_id(self, ctx: commands.Context, channelId: int):
         channel = self.bot.get_channel(channelId)
         if channel is not None:
             self.logger.debug(f'Saving server [{str(ctx.guild.id)}] to use channel id [{channelId}].')
@@ -97,13 +101,11 @@ class DailyLC(commands.Cog):
             await ctx.send(message)
             self.logger.info(message)
 
-    def save_channel_cache(self, guildId, channelId):
+    def save_channel_cache(self, guildId: int, channelId: int):
         server = ServerEntity(guildId, channelId)
         self.serverCache.save_entity(server)
 
-    def load_channel_cache(self, guildId):
+    def load_channel_cache(self, guildId: int) -> int:
         server = self.serverCache.load_entity(guildId)
-        if (server is None):
-            return 0
 
-        return server.channelId
+        return 0 if server is None else server.channelId
