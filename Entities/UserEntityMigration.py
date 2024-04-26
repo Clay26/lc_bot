a0993a8e57412ecf1ac60e2b71812359ef6bd50b
clay <5711623+Clay26@users.noreply.github.com>
2024-04-26 11:43:42 -0700

    Initial add of new User entity

---
+++ Entities/UserEntityMigration.py
@@ -0,0 +1,85 @@
+import datetime
+from azure.data.tables import TableEntity
+from typing import ClassVar, Type, Optional
+from dataclasses import dataclass
+from .BaseEntity import BaseEntity
+
+
+@dataclass
+class UserEntityMigration(BaseEntity):
+    userId: int
+    numEasy: int
+    numMedium: int
+    numHard: int
+    longestStreak: int
+    currStreakStartDate: Optional[datetime.date] = None
+    completedToday: bool
+
+    def __init__(self, userId: int):
+        super().__init__(str(userId), "stats")
+        self.userid = userId
+        self.numEasy = 0
+        self.numMedium = 0
+        self.numHard = 0
+        self.longestStreak = 0
+        self.currStreakStartDate = None
+        self.completedToday = False
+
+    def to_entity(self) -> dict:
+        currStreakStartDataStr = (self.currStreakStartDate.isoformat() if self.currStreakStartDate else "None")
+
+        return {
+            "PartitionKey": self.PartitionKey,
+            "RowKey": str(self.RowKey),
+            "numEasy": self.numEasy,
+            "numMedium": self.numMedium,
+            "numHard": self.numHard,
+            "longestStreak": self.longestStreak,
+            "currStreakStartDate": currStreakStartDataStr,  # ISO format for datetime
+            "completedToday": self.completedToday
+        }
+
+    @classmethod
+    def from_entity(cls: Type['UserEntity'], entity: TableEntity) -> 'UserEntity':
+        user_id = int(entity['RowKey'])
+        obj = cls(user_id)
+        obj.numEasy = int(entity.get('numEasy', 0))
+        obj.numMedium = int(entity.get('numMedium', 0))
+        obj.numHard = int(entity.get('numHard', 0))
+        obj.longestStreak = int(entity.get('longestStreak', 0))
+        obj.completedToday = entity.get('completedToday', False)
+
+        currStreakStartDateData = entity['currStreakStartDate']
+        if currStreakStartDateData == "None":
+            currStreakStartDate = None
+        else:
+            try:
+                currStreakStartDate = datetime.date.fromisoformat(currStreakStartDateData)
+            except ValueError:
+                try:
+                    currStreakStartDate = datetime.datetime.fromisoformat(currStreakStartDateData).date()
+                except ValueError:
+                    currStreakStartDate = datetime.datetime.now().date()
+            finally:
+                obj.currStreakStartDate = currStreakStartDate
+        return obj
+
+    def get_current_streak(self) -> int:
+        if (self.currStreakStartDate is None):
+            return 0
+
+        utc = datetime.timezone.utc
+        now = datetime.datetime.now(utc)
+        time = datetime.time(hour=11, minute=00, tzinfo=utc)
+
+        nextRelease = datetime.datetime.combine(now.date(), time)
+
+        if now > nextRelease:
+            # Before 11 AM UTC, use yesterday as latest release
+            nextRelease = nextRelease + datetime.timedelta(days=1)
+
+        latestRelease = nextRelease - datetime.timedelta(days=1)
+
+        todayBonus = (0 if self.completedToday else -1)
+
+        return (latestRelease.date() - self.currStreakStartDate).days + 1 + todayBonus
