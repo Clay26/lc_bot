a0f2470a45859666cbab5c2e95a8d2329b7426d0
clay <5711623+Clay26@users.noreply.github.com>
2025-02-02 15:43:28 -0500

    Add health endpoint

--- lc-bot-server.py
+++ lc-bot-server.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def home():
     return "Healthy!"
 
+@app.route("/amihealthy")
+def health_check():
+    return jsonify({"status": "healthy"}), 200
+
 def run_server():
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8000)))
 
